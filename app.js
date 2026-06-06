cd05f2df2605483727e7cb4e314d97bd65ca77f3
Shravan Gulekar <shravangulekar181206@gmail.com>
2026-06-06 12:59:11 +0530

    script refactored

--- app.js
+++ app.js
@@ -1,30 +1,70 @@
+//API
 const apiKey = "b790b03735c3448c9a2a8215bccaa62e";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+//DOM elements
+const panel = document.querySelector(".panel");
+
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+
 const weatherIcon = document.querySelector(".weather-icon");
+
+const cityEl = document.querySelector(".city");
+const tempEl =  document.querySelector(".temp");
+const humidityEl = document.querySelector(".humidity");
+const windEl = document.querySelector(".wind");
+
 const error = document.querySelector(".error");
 const weather = document.querySelector(".weather");
 
-
-function weatherIconImg(weather){
-    switch(weather){
-        case "Clouds":
-            return("images/clouds.png");
-        case "Clear":
-            return("images/clear.png");
-        case "Drizzle":
-            return("images/drizzle.png");
-        case "Mist":
-            return("images/mist.png");
-        case "Rain":
-            return("images/rain.png");
-        case "Snow":
-            return("images/snow.png");
-    }
+const weatherIcons = {
+    Clouds: "images/clouds.png",
+    Clear: "images/clear.png",
+    Drizzle: "images/drizzle.png",
+    Mist: "images/mist.png",
+    Rain: "images/rain.png",
+    Snow: "images/snow.png"
 }
 
+//API call
+async function fetchWeather(city){
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+    return await response.json();
+}
+
+//Rendering/Updating UI
+function renderUI(data){
+    cityEl.textContent = data.name;
+    tempEl.textContent = Math.round(data.main.temp)+ "°C";
+    humidityEl.textContent = data.main.humidity+ " %";
+    windEl.textContent = data.wind.speed+ " km/h";
+
+    weatherIcon.src = weatherIcons[data.weather[0].main];
+}
+
+function expandPanel(){
+    panel.style.height = panel.scrollHeight + "px";
+}
+
+async function  checkWeather(city) {
+    city = city.trim();
+
+    if(!city){
+        panel.style.height = "130px";
+        return
+    };
+
+    const data = await fetchWeather(city);
+
+    renderUI(data);
+
+    requestAnimationFrame(() => {
+        expandPanel();
+    });
+}
+
+//Events 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
@@ -33,25 +73,4 @@ searchBox.addEventListener("keydown", (event) => {
         checkWeather(searchBox.value);
 })
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
-    if ((response.status == 404||(response.status == 400))){
-        error.style.display = "block";
-        weather.style.display = "none";
-    }
-    else{
-        error.style.display = "none";
-        weather.style.display = "block";
-    }
-
-    let data = await response.json();
-
-    document.querySelector(".city").textContent = data.name;
-    document.querySelector(".temp").textContent = Math.round(data.main.temp)+ "°C";
-    document.querySelector(".humidity").textContent = data.main.humidity+ " %";
-    document.querySelector(".wind").textContent = data.wind.speed+ " km/h";
-
-    weatherIcon.src = weatherIconImg(data.weather[0].main)
-    weather.style.display = "block";
-}
-
+//
